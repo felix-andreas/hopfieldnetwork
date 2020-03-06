321badce794bd7a2d28f95a28b95f3771ed53bbd
Felix Andreas <felix.andreas95@googlemail.com>
2020-03-06 01:00:09 +0100

    push all

--- hopfieldnetwork/__init__.py
+++ hopfieldnetwork/__init__.py
@@ -1,8 +1,6 @@
+from .__about__ import __version__
 from .utils import *
 from .libary import *
-from .gui import GUI
 
-__version__ = 1.0
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), 'data')
-
+DATA_DIR = os.path.join(os.path.dirname(BASE_DIR), "data")
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(base_path, "README.md")) as file:
+    readme = file.read()
+
+with open(os.path.join(base_path, "hopfieldnetwork", "__about__.py")) as file:
+    about = {}
+    exec(file.read(), about)
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url=about["__url__"],
+    author=about["__author__"],
+    license=about["__license__"],
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib"],
+    python_requires=">=2.7",
+    include_package_data=True,
+    package_data={"hopfieldnetwork": ["data/**"],},
+    entry_points={
+        "console_scripts": ["hopfieldnetwork-ui=hopfieldnetwork.gui:start_gui"]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+    ],
+)
