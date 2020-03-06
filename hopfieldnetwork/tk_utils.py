c8c7262b700adf429b3877e04998a7292ebb12d8
Felix Andreas <felix.andreas95@googlemail.com>
2020-03-06 01:29:11 +0100

    Release on PyPI (#2)

--- hopfieldnetwork/tk_utils.py
+++ hopfieldnetwork/tk_utils.py
@@ -1,5 +1,6 @@
 # tk utilities
 import sys
+
 try:  # Python2
     import Tkinter as tk
     import tkFileDialog
@@ -9,22 +10,29 @@ except ImportError:  # Python3
 
 
 class CreateToolTip(object):
-    def __init__(self, widget, text='widget info'):
+    def __init__(self, widget, text="widget info"):
         self.widget = widget
         self.text = text
         self.widget.bind("<Enter>", self.enter)
         self.widget.bind("<Leave>", self.close)
 
     def enter(self, event=None):
-        x, y, cx, cy = self.widget.bbox("insert")
+        x, y, *_ = self.widget.bbox("insert")
         x += self.widget.winfo_rootx() + 0.1 * self.widget.winfo_width()
         y += self.widget.winfo_rooty() + self.widget.winfo_height()
-        # creates a toplevel window
+        # creates a toplevel master
         self.tw = tk.Toplevel(self.widget)
-        # Leaves only the label and removes the app window
+        # Leaves only the label and removes the gui master
         self.tw.wm_overrideredirect(True)
         self.tw.wm_geometry("+%d+%d" % (x, y))
-        label = tk.Label(self.tw, text=self.text, justify='left', background='white', relief='solid', borderwidth=1)
+        label = tk.Label(
+            self.tw,
+            text=self.text,
+            justify="left",
+            background="white",
+            relief="solid",
+            borderwidth=1,
+        )
         label.pack(ipadx=1)
 
     def close(self, event=None):
@@ -36,25 +44,25 @@ class ScrollSpinbox(tk.Spinbox):
     def __init__(self, *args, **kwargs):
         tk.Spinbox.__init__(self, *args, **kwargs)
         # Windows and macOS
-        self.bind('<MouseWheel>', self.mouseWheel)
+        self.bind("<MouseWheel>", self.mouseWheel)
         # Linux
-        self.bind('<Button-4>', self.mouseWheel)
-        self.bind('<Button-5>', self.mouseWheel)
+        self.bind("<Button-4>", self.mouseWheel)
+        self.bind("<Button-5>", self.mouseWheel)
 
     def mouseWheel(self, event):
         # print(event.delta, event.num)
         if event.num == 5 or event.delta < 0:
-            self.invoke('buttondown')
+            self.invoke("buttondown")
         elif event.num == 4 or event.delta > 0:
-            self.invoke('buttonup')
+            self.invoke("buttonup")
 
 
 # check OS
 def checkOS():
     if sys.platform == "linux" or sys.platform == "linux2":
-        operatingsystem = 'linux'
+        operatingsystem = "linux"
     elif sys.platform == "darwin":
-        operatingsystem = 'macOS'
+        operatingsystem = "macOS"
     elif sys.platform == "win32":
-        operatingsystem = 'windows'
+        operatingsystem = "windows"
     return operatingsystem
